a799a45d307c4344e331bf93e7d5bd13e7e52965
hk1 <hk@ipunkt.biz>
2020-02-17 15:54:09 +0100

    Translation added

---
+++ Locale/de_DE/translations.php
@@ -0,0 +1,4 @@
+<?php
+return array(
+    'This plugin offers the possibility to focus on Lightbox when the site is loaded.' => 'Dieses Plugin bietet die Möglichkeit, ein Fokus bei Lightbox zu setzen',
+);
